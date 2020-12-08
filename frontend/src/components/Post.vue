db0dc3fe2a886af920a140255287bd38890d686f
Marie Paret <marieparet.33@gmail.com>
2020-12-08 10:50:31 +0100

    implement likes counter in front

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -22,11 +22,26 @@
 
       <b-card-text class="text-left mt-3">{{ post.content }}</b-card-text>
 
-      <span class="post justify-content-center">
+      <span class="post justify-content-center mb-4">
         <img class="post__image" :src="post.imageUrl" />
       </span>
 
-      <div class="line mt-5"></div>
+      <div v-if="likesCount > 0" class="d-flex mb-3 ml-2 text-left">
+        <div
+          class="svg-container d-flex justify-content-center align-items-center"
+        >
+          <svg style="width:17px;height:17px" viewBox="0 0 24 24">
+            <path
+              fill="#fff"
+              d="M23,10C23,8.89 22.1,8 21,8H14.68L15.64,3.43C15.66,3.33 15.67,3.22 15.67,3.11C15.67,2.7 15.5,2.32 15.23,2.05L14.17,1L7.59,7.58C7.22,7.95 7,8.45 7,9V19A2,2 0 0,0 9,21H18C18.83,21 19.54,20.5 19.84,19.78L22.86,12.73C22.95,12.5 23,12.26 23,12V10M1,21H5V9H1V21Z"
+            />
+          </svg>
+        </div>
+        <span class="ml-2">{{ likesCount }}</span>
+      </div>
+
+      <div class="line"></div>
+
       <div class="footer d-flex justify-content-around">
         <button
           @click="likeOrUnlikePost"
@@ -92,12 +107,18 @@ export default {
   },
   data () {
     return {
-      likesThisPost: false
+      likesThisPost: false,
+      likesCount: this.post.likesCount
     }
   },
   methods: {
     async likeOrUnlikePost () {
       const res = await apiClient.post(`api/posts/${this.post.id}/likes`)
+
+      if (res.like !== this.likesThisPost) {
+        this.likesCount += res.like ? 1 : -1
+      }
+
       this.likesThisPost = res.like
     },
     displayNotification (text) {
@@ -152,5 +173,12 @@ export default {
     color: #747474 !important;
     background-color: rgba(108, 117, 125, 0.1) !important;
   }
+}
+
+.svg-container {
+  border-radius: 100%;
+  background-color: rgb(32, 120, 244);
+  width: 25px;
+  height: 25px;
 }
 </style>
