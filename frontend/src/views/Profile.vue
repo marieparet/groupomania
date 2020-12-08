0ae8c6357777ca2bd9f0654a8cc5400ed5a14a8d
Marie Paret <marieparet.33@gmail.com>
2020-12-08 11:17:42 +0100

    improve profile edition design

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -13,20 +13,28 @@
               customClass="profile-main-picture"
             />
             <b-form-group>
+              <b-button
+                class="btn-block w-50 mx-auto mb-3 justify-content-center"
+                @click="triggerInput"
+              >
+                <span class="button-text mr-2">Changer ma photo de profil</span>
+                <svg
+                  style="width:24px;height:24px;margin-bottom:5px"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="rgba(253, 45, 6, 0.5)"
+                    d="M15.5,9C16.2,9 16.79,8.76 17.27,8.27C17.76,7.79 18,7.2 18,6.5C18,5.83 17.76,5.23 17.27,4.73C16.79,4.23 16.2,4 15.5,4C14.83,4 14.23,4.23 13.73,4.73C13.23,5.23 13,5.83 13,6.5C13,7.2 13.23,7.79 13.73,8.27C14.23,8.76 14.83,9 15.5,9M19.31,8.91L22.41,12L21,13.41L17.86,10.31C17.08,10.78 16.28,11 15.47,11C14.22,11 13.16,10.58 12.3,9.7C11.45,8.83 11,7.77 11,6.5C11,5.27 11.45,4.2 12.33,3.33C13.2,2.45 14.27,2 15.5,2C16.77,2 17.83,2.45 18.7,3.33C19.58,4.2 20,5.27 20,6.5C20,7.33 19.78,8.13 19.31,8.91M16.5,18H5.5L8.25,14.5L10.22,16.83L12.94,13.31L16.5,18M18,13L20,15V20C20,20.55 19.81,21 19.41,21.4C19,21.79 18.53,22 18,22H4C3.45,22 3,21.79 2.6,21.4C2.21,21 2,20.55 2,20V6C2,5.47 2.21,5 2.6,4.59C3,4.19 3.45,4 4,4H9.5C9.2,4.64 9.03,5.31 9,6H4V20H18V13Z"
+                  /></svg
+              ></b-button>
               <div class="d-flex align-items-center">
-                <b-col sm="2">
-                  <label for="imageUrl">
-                    Photo
-                  </label>
-                </b-col>
                 <b-col sm="10">
-                  <b-form-file
-                    ref="fileupload"
-                    id="imageUrl"
-                    class="text-dark mb-2 pl-3"
-                    placeholder="Aucun fichier selectionné"
+                  <input
+                    ref="fileUpload"
+                    class="d-none"
+                    type="file"
                     @change="onFileSelected"
-                  ></b-form-file>
+                  />
                 </b-col>
               </div>
               <div class="d-flex align-items-center">
@@ -73,7 +81,7 @@
                 </b-col>
               </div>
             </b-form-group>
-            <b-button type="submit" variant="primary">Enregistrer</b-button>
+            <b-button type="submit" class="save-btn">Enregistrer</b-button>
           </b-form>
         </b-card>
       </b-col>
@@ -115,6 +123,11 @@ export default {
       this.url = URL.createObjectURL(event.target.files[0])
       this.selectedFile = event.target.files[0]
     },
+
+    triggerInput () {
+      this.$refs.fileUpload.click()
+    },
+
     editUser (event) {
       let body = this.input
 
@@ -135,7 +148,6 @@ export default {
         })
         window.location.reload()
       })
-      this.$refs.fileupload.reset()
     }
   }
 }
@@ -151,10 +163,20 @@ export default {
   width: 100px;
   height: 100px;
   border-radius: 100%;
-  margin-bottom: 2rem;
+  margin-bottom: 1rem;
 }
 
 .custom-file-label {
   text-align: left;
+}
+
+.save-btn {
+  background-color: rgba(253, 45, 6, 0.8);
+  color: white;
+  border-radius: 1rem;
+  &:hover {
+    background-color: rgb(253, 45, 6) !important;
+    color: white !important;
+  }
 }
 </style>
